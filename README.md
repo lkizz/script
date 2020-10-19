66fc04228d6b88f88ecdc2c4c96852fb9da4a150
匿名である <anonymous@t.cn>
2020-10-19 22:28:55 +0800

    支持指定某个账号不执行刷时长

--- README.md
+++ README.md
@@ -71,6 +71,11 @@ FORK后，如果actions没有定时执行，请随便找个文件，加个空格
 | `XMLY_SPEED_COOKIE`     | 喜马拉雅 | 非必须 | [Cookie获取请参考](https://github.com/Zero-S1/xmly_speed/blob/master/xmly_speed.md),仅支持git actions执行,多个Cookie用换行即可 |
 | `XMLY_ANDROID_AGENT`     | 喜马拉雅 | 非必须 | 仅安卓用的Agent配置，不填的话也会默认用红米8的 |
 | `XMLY_ACCUMULATE_TIME`     | 喜马拉雅 | 非必须 | 需要刷时长任务的话，填入`zero_s1`；可能会黑号，请知悉 |    
+| `XMLY_ACCUMULATE_INDEX`     | 喜马拉雅 | 非必须 | 需配合`XMLY_ACCUMULATE_TIME`使用，用于限定某个索引的账号不进行刷时长 |
+
+#### 关于XMLY_ACCUMULATE_INDEX
+比如我有5个账号，我第1个和第5个不想执行刷时长任务，则填入内容为`0,4`
+例如我只有两个号，第2个号不想执行刷时长，则直接填入`1`即可
 
 #### 关于分享码的说明:
 
--- xmly_speed.py
+++ xmly_speed.py
@@ -14,6 +14,9 @@ def run():
     webFileContent = loadFileContent(os.environ["SYNCURL"])
     print("\n文件同步完毕, 处理中...")
     xmly_speed_cookie = os.environ["XMLY_SPEED_COOKIE"]
+    xmly_accumulate_index=[]
+    if not os.environ["XMLY_ACCUMULATE_INDEX"].strip()=='':
+       xmly_accumulate_index = [int(i) for i in os.environ["XMLY_ACCUMULATE_INDEX"].split(",")]
     if len(xmly_speed_cookie)==0 or xmly_speed_cookie.strip()=='':
         print("无法获取Cookie,请在Secrets中配置XMLY_SPEED_COOKIE")
         return
@@ -25,6 +28,15 @@ def run():
             if len(rewriteAgent) ==0 or rewriteAgent.strip()=='':
                 rewriteAgent='UserAgent = "ting_1.8.30(Redmi+7,Android28)"'
                 executeContent = re.sub(agentPattern,rewriteAgent,executeContent)
+        if len(xmly_accumulate_index)>0:
+            index = -1
+            try:
+                index = xmly_accumulate_index.index(idx)
+            except ValueError:
+                print("无需禁用")
+            if index>=0:
+                executeContent = executeContent.replace("XMLY_ACCUMULATE_TIME = 1","XMLY_ACCUMULATE_TIME = 0",1).replace("action 自动刷时长打开","action 自动刷时长被指定关闭",1)
+                print("已接受XMLY_ACCUMULATE_INDEX配置,为索引"+str(idx)+"的数据执行了禁用刷新时长的操作")
         writeFile(executeContent,'execute'+str(idx)+'.py')
         os.system('python ./'+'execute'+str(idx)+'.py')
     print("\n***************************\n文件全部执行完毕")
