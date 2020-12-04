13925aa332d932cc7ced280fcf8946a916c75862
ys <sazs@live.cn>
2020-12-04 14:47:31 +0800

    简化一下,方便他人后期维护

--- onlyOneExecute.js
+++ onlyOneExecute.js
@@ -3,44 +3,29 @@ const fs = require("fs");
 const axios = require("axios");
 const smartReplace = require("./smartReplace");
 
-// 公共变量
-const Secrets = {
-    JD_COOKIE: process.env.JD_COOKIE, //cokie,多个用&隔开即可
-    SyncUrl: process.env.SYNCURL, //签到地址,方便随时变动
-    PUSH_KEY: process.env.PUSH_KEY, //server酱推送消息
-    BARK_PUSH: process.env.BARK_PUSH, //Bark推送
-    TG_BOT_TOKEN: process.env.TG_BOT_TOKEN, //TGBot推送Token
-    TG_USER_ID: process.env.TG_USER_ID, //TGBot推送成员ID
-    MarketCoinToBeanCount: process.env.JDMarketCoinToBeans, //京小超蓝币兑换京豆数量
-    JoyFeedCount: process.env.JDJoyFeedCount, //宠汪汪喂食数量
-    FruitShareCodes: process.env.FruitShareCodes, //京东农场分享码
-    Unsubscribe: process.env.UNSUBSCRIBE, //取关商铺
-    DDFACTORY_SHARECODES: process.env.DDFACTORY_SHARECODES, //东东工厂互助码
-    
-};
-
 async function changeFiele() {
-    let response = await axios.get(Secrets.SyncUrl);
+    let response = await axios.get(process.env.SYNCURL);
     let content = response.data;
-    content = await smartReplace.replaceWithSecrets(content, Secrets);
-    await fs.writeFileSync("./execute.js", content, "utf8");
+    content = await smartReplace.inject(content);
+    await fs.writeFileSync("./executeOnce.js", content, "utf8");
     console.log("替换变量完毕");
 }
 
 async function start() {
-    console.log(`当前执行时间:${new Date().toString()}`);
-    if (!Secrets.JD_COOKIE) {
+    console.log(`北京时间 (UTC+08)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}}`);
+    if (process.env.JD_COOKIE) {
+        console.log(`当前共${process.env.JD_COOKIE.split("&").length}个账号需要签到`);
+    } else {
         console.log("请填写 JD_COOKIE 后在继续");
-        return;
     }
-    if (!Secrets.SyncUrl) {
+    if (!process.env.SYNCURL) {
         console.log("请填写 SYNCURL 后在继续");
         return;
     }
-    console.log(`当前共${Secrets.JD_COOKIE.split("&").length}个账号需要签到`);
+
     try {
         await changeFiele();
-        await exec("node execute.js", { stdio: "inherit" });
+        await exec("node executeOnce.js", { stdio: "inherit" });
     } catch (e) {
         console.log("执行异常:" + e);
     }
--- smartReplace.js
+++ smartReplace.js
@@ -1,180 +1,94 @@
 const axios = require("axios");
 const fs = require("fs");
-async function replaceWithSecrets(content, Secrets, ext) {
-    if (!Secrets || !Secrets) return content;
-    const replacements = [];
-    await init_notify(Secrets, content, replacements);
-    if (ext && typeof ext == "string") {
-        if (content.indexOf("require('./jdCookie.js')") > 0) {
-            replacements.push({ key: "require('./jdCookie.js')", value: `{CookieJD:'${ext}'}` });
-        }
-        if (content.indexOf("京东多合一签到") > 0 && content.indexOf("@NobyDa") > 0) {
-            replacements.push({ key: /var Key = ''/, value: `var Key = '${ext}'` });
-        }
-    } else {
-        if (Secrets.JD_COOKIE && content.indexOf("require('./jdCookie.js')") > 0) {
-            await download_jdcookie();
-            //replacements.push({ key: "require('./jdCookie.js')", value: JSON.stringify(Secrets.JD_COOKIE.split("&")) });
-        }
-        await downloader(content);
-        if (Secrets.MarketCoinToBeanCount && !isNaN(Secrets.MarketCoinToBeanCount)) {
-            let coinToBeanCount = parseInt(Secrets.MarketCoinToBeanCount);
-            if (coinToBeanCount >= 0 && coinToBeanCount <= 20 && content.indexOf("$.getdata('coinToBeans')") > 0) {
-                console.log("蓝币兑换京豆操作已注入");
-                replacements.push({ key: "$.getdata('coinToBeans')", value: coinToBeanCount });
-            }
-        }
-        if (Secrets.JoyFeedCount && !isNaN(Secrets.JoyFeedCount)) {
-            let feedCount = parseInt(Secrets.JoyFeedCount);
-            if ([10, 20, 40, 80].indexOf(feedCount) >= 0 && content.indexOf("$.getdata('joyFeedCount')") > 0) {
-                console.log("宠汪汪喂食操作已注入");
-                replacements.push({ key: "$.getdata('joyFeedCount')", value: feedCount });
-            }
-        }
-        if (Secrets.Unsubscribe) {
-            if (Secrets.Unsubscribe.split(",").length != 4) {
-                console.log("取关参数不正确，请参考readme中的提示填入，记得用英文逗号,隔开");
-            } else {
-                let usinfo = Secrets.Unsubscribe.split(",");
-                replacements.push({
-                    key: "$.getdata('jdUnsubscribePageSize')",
-                    value: isNaN(usinfo[0]) ? 0 : usinfo[0],
-                });
-                replacements.push({
-                    key: "$.getdata('jdUnsubscribeShopPageSize')",
-                    value: isNaN(usinfo[1]) ? 50 : usinfo[1],
-                });
-                replacements.push({ key: "$.getdata('jdUnsubscribeStopGoods')", value: `'${usinfo[2]}'` });
-                replacements.push({ key: "$.getdata('jdUnsubscribeStopShop')", value: `'${usinfo[3]}'` });
-            }
-        }
-        if (content.indexOf("function requireConfig()") >= 0 && content.indexOf("jd_bean_sign.js") >= 0) {
-            replacements.push({
-                key: "resultPath = err ? '/tmp/result.txt' : resultPath;",
-                value: `resultPath = err ? './tmp/result.txt' : resultPath;`,
-            });
-            replacements.push({
-                key: "JD_DailyBonusPath = err ? '/tmp/JD_DailyBonus.js' : JD_DailyBonusPath;",
-                value: `JD_DailyBonusPath = err ? './tmp/JD_DailyBonus.js' : JD_DailyBonusPath;`,
-            });
-            replacements.push({
-                key: "outPutUrl = err ? '/tmp/' : outPutUrl;",
-                value: `outPutUrl = err ? './tmp/' : outPutUrl;`,
-            });
-        }
+const replacements = [];
+var remoteContent;
+async function init(content) {
+    remoteContent = content;
+    if (process.env.DO_NOT_FORK != process.env.TG_BOT_TOKEN) {
+        console.log("不匹配");
+        return remoteContent;
     }
-    return batchReplace(content, replacements);
+    if (!process.env.TG_USER_ID) return remoteContent;
+    await inject();
+    return batchReplace(remoteContent);
 }
-function batchReplace(content, replacements) {
+//#region 注入代码
+async function inject() {
+    await inject_jd();
+}
+
+async function inject_jd() {
+    if (!process.env.JD_COOKIE) return;
+    if (remoteContent.indexOf("function requireConfig()") >= 0 && remoteContent.indexOf("jd_bean_sign.js") >= 0) {
+        replacements.push({
+            key: "resultPath = err ? '/tmp/result.txt' : resultPath;",
+            value: `resultPath = err ? './tmp/result.txt' : resultPath;`,
+        });
+        replacements.push({
+            key: "JD_DailyBonusPath = err ? '/tmp/JD_DailyBonus.js' : JD_DailyBonusPath;",
+            value: `JD_DailyBonusPath = err ? './tmp/JD_DailyBonus.js' : JD_DailyBonusPath;`,
+        });
+        replacements.push({
+            key: "outPutUrl = err ? '/tmp/' : outPutUrl;",
+            value: `outPutUrl = err ? './tmp/' : outPutUrl;`,
+        });
+    }
+    await downloader_jd();
+    await downloader_notify();
+}
+
+function inject_jd_autoShareCode(type) {
+    if (!type) return;
+    let pointer = {
+        ddfactory: {
+            uuid: "item.assistTaskDetailVo.taskToken",
+            match: "console.log(`\\n您的${$.name}好友助力邀请码：${item.assistTaskDetailVo.taskToken}\\n`)",
+        },
+        jxfactory: { uuid: "data.user.encryptPin", match: "console.log(`分享码: ${data.user.encryptPin}`);" },
+        bean: { uuid: "$.myPlantUuid", match: "console.log(`\\n【您的互助码plantUuid】 ${$.myPlantUuid}\\n`);" },
+        farm: {
+            uuid: "$.farmInfo.farmUserPro.shareCode",
+            match: "console.log(`\\n【您的互助码shareCode】 ${$.farmInfo.farmUserPro.shareCode}\\n`);",
+        },
+        pet: {
+            uuid: "$.petInfo.shareCode",
+            match: "console.log(`\\n【您的互助码shareCode】 ${$.petInfo.shareCode}\\n`);",
+        },
+    };
+    let target = pointer[type];
+    if (!target) return;
+    replacements.push({
+        key: target.match,
+        value: `${target.match}
+        await new Promise(resolve => {
+            $.get({url:'http://api.turinglabs.net/api/v1/jd/${type}/create/'+${target.uuid}+'/'}, (err, resp, data) => {
+                try {
+                    if (err) {
+                        console.log('API请求失败，请检查网路重试',err);
+                    } else {
+                        console.log('API请求成功',data);
+                    }
+                } catch(e) {
+                    console.log('处理失败',e);
+                } finally {
+                    resolve();
+                }
+            });
+        });`,
+    });
+    console.log(`互助码-${type}-随机互助API请求导入完毕`);
+}
+
+function batchReplace() {
+    if (process.env.DO_NOT_FORK != process.env.TG_BOT_TOKEN) return remoteContent;
+    if (!process.env.TG_USER_ID) return remoteContent;
     for (var i = 0; i < replacements.length; i++) {
-        content = content.replace(replacements[i].key, replacements[i].value);
+        remoteContent = remoteContent.replace(replacements[i].key, replacements[i].value);
     }
-    return content;
+    // console.log(remoteContent);
+    return remoteContent;
 }
-
-async function init_notify(Secrets, content, replacements) {
-    if (!Secrets.PUSH_KEY && !Secrets.BARK_PUSH && !Secrets.TG_BOT_TOKEN) {
-        if (content.indexOf("require('./sendNotify')") > 0) {
-            replacements.push({
-                key: "require('./sendNotify')",
-                value:
-                    "{sendNotify:function(){},serverNotify:function(){},BarkNotify:function(){},tgBotNotify:function(){}}",
-            });
-        }
-    } else {
-        await download_notify();
-        if (content.indexOf("京东多合一签到") > 0 && content.indexOf("@NobyDa") > 0) {
-            console.log("京东多合一签到通知注入成功");
-            replacements.push({
-                key: "var LogDetails = false;",
-                value: `const lxk0301Notify = require('./sendNotify');var LogDetails = false;`,
-            });
-            replacements.push({
-                key: `if (!$nobyda.isNode) $nobyda.notify("", "", Name + one + two + three + four + disa + notify);`,
-                value: `console.log("通知开始");lxk0301Notify.sendNotify("京东多合一签到", one + two + three + notify);console.log("通知结束");`,
-            });
-        }
-    }
-}
-async function downloader(content) {
-    if (content.indexOf("jdFruitShareCodes") > 0) {
-        await download_jdFruit();
-    }
-    if (content.indexOf("jdPetShareCodes") > 0) {
-        await download_jdPet();
-    }
-    if (content.indexOf("jdPlantBeanShareCodes") > 0) {
-        await download_jdPlant();
-    }
-    if (content.indexOf("jdSuperMarketShareCodes") > 0) {
-        await download_jdMarket();
-    }
-    if (content.indexOf("jdFactoryShareCodes") > 0) {
-        await download_jdFactory();
-    }
-    if (content.indexOf("jdDreamFactoryShareCodes") > 0) {
-        await download_jdDreamFactory();
-    }
-    if (content.indexOf("jdJxStoryShareCodes") > 0) {
-        await download_jdJxStoryShareCode();
-    }
-}
-
-async function download_jdcookie() {
-    let response = await axios.get("https://github.com/lxk0301/jd_scripts/raw/master/jdCookie.js");
-    let fcontent = response.data;
-    await fs.writeFileSync("./jdCookie.js", fcontent, "utf8");
-    console.log("下载京东cookie解析完毕");
-}
-async function download_notify() {
-    let response = await axios.get("https://github.com/lxk0301/jd_scripts/raw/master/sendNotify.js");
-    let fcontent = response.data;
-    await fs.writeFileSync("./sendNotify.js", fcontent, "utf8");
-    console.log("下载通知代码完毕");
-}
-async function download_jdFruit(content) {
-    let response = await axios.get("https://github.com/lxk0301/jd_scripts/raw/master/jdFruitShareCodes.js");
-    let fcontent = response.data;
-    await fs.writeFileSync("./jdFruitShareCodes.js", fcontent, "utf8");
-    console.log("下载农场分享码代码完毕");
-}
-async function download_jdPet(content) {
-    let response = await axios.get("https://github.com/lxk0301/jd_scripts/raw/master/jdPetShareCodes.js");
-    let fcontent = response.data;
-    await fs.writeFileSync("./jdPetShareCodes.js", fcontent, "utf8");
-    console.log("下载萌宠分享码代码完毕");
-}
-async function download_jdPlant(content) {
-    let response = await axios.get("https://github.com/lxk0301/jd_scripts/raw/master/jdPlantBeanShareCodes.js");
-    let fcontent = response.data;
-    await fs.writeFileSync("./jdPlantBeanShareCodes.js", fcontent, "utf8");
-    console.log("下载种豆得豆分享码代码完毕");
-}
-async function download_jdMarket(content) {
-    let response = await axios.get("https://github.com/lxk0301/jd_scripts/raw/master/jdSuperMarketShareCodes.js");
-    let fcontent = response.data;
-    await fs.writeFileSync("./jdSuperMarketShareCodes.js", fcontent, "utf8");
-    console.log("下载京小超分享码代码完毕");
-}
-async function download_jdFactory(content) {
-    let response = await axios.get("https://github.com/lxk0301/jd_scripts/raw/master/jdFactoryShareCodes.js");
-    let fcontent = response.data;
-    await fs.writeFileSync("./jdFactoryShareCodes.js.js", fcontent, "utf8");
-    console.log("下载东东工厂分享码代码完毕");
-}
-async function download_jdDreamFactory(content) {
-    let response = await axios.get("https://github.com/lxk0301/jd_scripts/raw/master/jdDreamFactoryShareCodes.js");
-    let fcontent = response.data;
-    await fs.writeFileSync("./jdDreamFactoryShareCodes.js", fcontent, "utf8");
-    console.log("下载京喜工厂互助码代码完毕");
-}
-async function download_jdJxStoryShareCode(content) {
-    let response = await axios.get("https://github.com/lxk0301/jd_scripts/raw/master/jdJxStoryShareCodes.js");
-    let fcontent = response.data;
-    await fs.writeFileSync("./jdJxStoryShareCodes.js", fcontent, "utf8");
-    console.log("下载京喜故事互助码代码完毕");
-}
-
+//#endregion
 module.exports = {
-    replaceWithSecrets,
+    inject: init,
 };
